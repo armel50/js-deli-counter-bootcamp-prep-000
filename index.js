0918c495707c495c67e61047cabc9fd22563a36a
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-26 15:43:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(lineOfpeople,newPerson){
 }
 
 function nowServing(katzDeliLine){
-  if(katzDeliLine.length=0){
+  if(katzDeliLine.length===0){
     return "There is nobody waiting to be served"
   }
   for(var i =0; i<katzDeliLine.length;i++){

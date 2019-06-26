5803c8fce3123718e9a8402a4708071d2756a447
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-26 16:24:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function takeANumber(lineOfpeople,newPerson){
 }
 
 function nowServing(katzDeliLine){
+  
   if(katzDeliLine.length===0){
     return "There is nobody waiting to be served!"
   }
@@ -14,14 +15,14 @@ function nowServing(katzDeliLine){
      console.log(`Currently serving ${katzDeliLine[i]}`) ;
     katzDeliLine.shift(katzDeliLine[i]);
   }
-   console.log(`Currently serving ${katzDeliLine[i]}`) ;
+   
 }
 
 function currentLine(katzDeliLine){
   if(katzDeliLine.length ===0){
-    return `The line is currently empty.`
+    return `The line is currently empty.`;
   }
   
-  return
+  
   
 }

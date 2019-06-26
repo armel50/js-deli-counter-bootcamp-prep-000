0f1267923ba6d2d887a80bc93d3b5db9aa9e662c
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-26 17:11:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,19 +5,14 @@ function takeANumber(lineOfpeople,newPerson){
   return `Welcome, ${newPerson}. You are number ${lineOfpeople.indexOf(newPerson)+1} in line.`;
   
 }
-var array=[];
-var array1 ;
-function shiftTheArray(){
-  if(array.length !==0){
-    array1 = array.shift();
-  return `Currently serving ${array1}`;
-  }
+
+
   
   
-}
+
 
 function nowServing(katzDeliLine){
-  array = katzDeliLine;
+
   if(array.length===0){
     return "There is nobody waiting to be served!";
   }else{
